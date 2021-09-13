14c751e4c518581cc21d4f5de1201e447f8d9cc5
Rakha <rakhawibowo1998@gmail.com>
2021-09-13 15:55:20 +0700

    Update

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -4,6 +4,15 @@ const Hero = () => {
   return (
     <div className="hero">
       <img src={hero} alt="" />
+      <h1>A Simple Bookmark Manager</h1>
+      <p>
+        A clean and simple interface to organize your favourite websites. Open a
+        new browser tab and see your sites load instantly. Try it for free.
+      </p>
+      <div className="button">
+        <button className="button1">Get it on Chrome</button>
+        <button className="button2">Get it on Firefox</button>
+      </div>
     </div>
   );
 };
