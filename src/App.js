09ed8302664143f69693c2199c0423e002a4afd5
Rakha <rakhawibowo1998@gmail.com>
2021-09-13 14:04:23 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./images/logo-bookmark.svg";
+import logo_putih from "./images/logo-bookmark-putih.svg";
+import hamburger from "./images/icon-hamburger.svg";
+import x from "./images/icon-close.svg";
+import fb from "./images/icon-facebook.svg";
+import tw from "./images/icon-twitter.svg";
 
 function App() {
+  const [aktif, setAktif] = useState(false);
+  const toggleClass = () => {
+    setAktif(!aktif);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={aktif ? "App aktif" : "App"}>
+      <div className="nav">
+        <img
+          className="logo"
+          src={aktif ? logo_putih : logo}
+          alt="logo-bookmark"
+        />
+        <img
+          onClick={() => toggleClass()}
+          className="ham"
+          src={aktif ? x : hamburger}
+          alt="icon-hamburger"
+        />
+      </div>
+      <div className={aktif ? "nav_aktif aktif" : "nav_aktif"}>
+        <div className="isi">
+          <p>FEATURES</p>
+          <p>PRICING</p>
+          <p>CONTACT</p>
+          <button>LOGIN</button>
+        </div>
+        <div className="sosmed">
+          <div className="icon">
+            <a
+              className="fb"
+              href="https://www.facebook.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={fb} alt="" />
+            </a>
+            <a href="https://twitter.com" target="_blank" rel="noreferrer">
+              <img src={tw} alt="" />
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
