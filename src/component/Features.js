787c16bb6ff3db2cd28583d3c877b97922c42bd9
Rakha <rakhawibowo1998@gmail.com>
2021-09-16 14:21:10 +0700

    Update

--- src/component/Features.js
+++ src/component/Features.js
@@ -38,42 +38,49 @@ function Features() {
         websites. Your bookmarks sync between your devices so you can access
         them on the go.
       </p>
-      <h2 className="f1" onClick={() => toggle1()}>
-        Simple Bookmarking
-      </h2>
-      <div className={aktif.k1 ? "merah aktif" : "merah"}></div>
-      <h2 className="f2" onClick={() => toggle2()}>
-        Speedy Searching
-      </h2>
-      <div className={aktif.k2 ? "merah aktif" : "merah"}></div>
-      <h2 className="f3" onClick={() => toggle3()}>
-        Easy Sharing
-      </h2>
-      <div className={aktif.k3 ? "merah aktif" : "merah"}></div>
-      <div className={aktif.k1 ? "konten aktif" : "konten"}>
-        <img src={k1} alt="" />
-        <h1>Bookmark in one click</h1>
-        <p>
-          Organize your bookmarks however you like. Our simple drag-and-drop
-          interface gives you complete control over how you manage your
-          favourite sites.
-        </p>
+      <div className="tab_fitur">
+        <h2 className="f1" onClick={() => toggle1()}>
+          Simple Bookmarking
+        </h2>
+        <div className={aktif.k1 ? "merah aktif" : "merah"}></div>
+        <h2 className="f2" onClick={() => toggle2()}>
+          Speedy Searching
+        </h2>
+        <div className={aktif.k2 ? "merah aktif" : "merah"}></div>
+        <h2 className="f3" onClick={() => toggle3()}>
+          Easy Sharing
+        </h2>
+        <div className={aktif.k3 ? "merah aktif" : "merah"}></div>
       </div>
-      <div className={aktif.k2 ? "konten aktif" : "konten"}>
-        <img src={k2} alt="" />
-        <h1>Intelligent search</h1>
-        <p>
-          Our powerful search feature will help you find saved sites in no time
-          at all. No need to trawl through all of your bookmarks.
-        </p>
-      </div>
-      <div className={aktif.k3 ? "konten aktif" : "konten"}>
-        <img src={k3} alt="" />
-        <h1>Share your bookmarks</h1>
-        <p>
-          Easily share your bookmarks and collections with others. Create a
-          shareable link that you can send at the click of a button.
-        </p>
+      <div className="isi_fitur">
+        <div className={aktif.k1 ? "konten aktif" : "konten"}>
+          <img src={k1} alt="" />
+          <div className="shape"></div>
+          <h1>Bookmark in one click</h1>
+          <p>
+            Organize your bookmarks however you like. Our simple drag-and-drop
+            interface gives you complete control over how you manage your
+            favourite sites.
+          </p>
+        </div>
+        <div className={aktif.k2 ? "konten aktif" : "konten"}>
+          <img src={k2} alt="" />
+          <div className="shape2"></div>
+          <h1>Intelligent search</h1>
+          <p>
+            Our powerful search feature will help you find saved sites in no
+            time at all. No need to trawl through all of your bookmarks.
+          </p>
+        </div>
+        <div className={aktif.k3 ? "konten aktif" : "konten"}>
+          <img src={k3} alt="" />
+          <div className="shape3"></div>
+          <h1>Share your bookmarks</h1>
+          <p>
+            Easily share your bookmarks and collections with others. Create a
+            shareable link that you can send at the click of a button.
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/component/Hero.js
+++ src/component/Hero.js
@@ -3,15 +3,20 @@ import hero from "../images/illustration-hero.svg";
 const Hero = () => {
   return (
     <div className="hero">
-      <img src={hero} alt="" />
-      <h1>A Simple Bookmark Manager</h1>
-      <p>
-        A clean and simple interface to organize your favourite websites. Open a
-        new browser tab and see your sites load instantly. Try it for free.
-      </p>
-      <div className="button">
-        <button className="button1">Get it on Chrome</button>
-        <button className="button2">Get it on Firefox</button>
+      <div className="hero_image">
+        <img src={hero} alt="" />
+        <div className="shape"></div>
+      </div>
+      <div className="hero_konten">
+        <h1>A Simple Bookmark Manager</h1>
+        <p>
+          A clean and simple interface to organize your favourite websites. Open
+          a new browser tab and see your sites load instantly. Try it for free.
+        </p>
+        <div className="button">
+          <button className="button1">Get it on Chrome</button>
+          <button className="button2">Get it on Firefox</button>
+        </div>
       </div>
     </div>
   );
